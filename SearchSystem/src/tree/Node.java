0dee5c6376768473f8fb6d0f82b69de8222b3aeb
Valmir Correa <valmircorrea96@outlook.com>
2017-11-14 21:46:15 -0300

    Iniciando implementação da arvore, usando a classe Node

--- SearchSystem/src/tree/Node.java
+++ SearchSystem/src/tree/Node.java
@@ -1,29 +1,48 @@
 /**
- * 
+ * Archive that contains a implementation of the class of node for one tree.
  */
 package tree;
 
 import java.awt.List;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * @author valmir
- *
+ * @author Valmir Correa
+ * @author
+ * @author
+ * Classs that represent a node of the tree.
  */
 public class Node {
 	
-	private Map<Charset, Node> childrens;
-	private boolean info;
-	private HashMap <List<String,Map.Entry<Integer, Integer>>> value;
+	private Map<Charset, Node> childrens;									// childrens of this node
+	private boolean info;													// info if a node form one word
+	private HashMap <String, ArrayList<Map.Entry<Integer, Integer>>> value;	// Contains a text, amount by line and line.
 
 	/**
-	 * 
+	 * Constructor 
 	 */
 	public Node() {
 		
-		node = new HashMap<Charset,Node>();
+		childrens = new HashMap<Charset, Node>();
+		info = false;
+		value = new HashMap<String, ArrayList<Map.Entry<Integer, Integer>>>();
 	}
-
+	
+	/**
+	 * Variable that informs if the way form one word.
+	 * @return information about word formed.
+	 */
+	public boolean getInfo () {
+		return info;
+	}
+	
+	@Override
+	public String toString () {
+		return null;
+		// a gente define algo aqui
+	}
+	
 }
---
+++ SearchSystem/src/tree/Trie.java
@@ -0,0 +1,51 @@
+/**
+ * Archieve that contains class defined a tree.
+ */
+package tree;
+
+/**
+ * @author Valmir Correa
+ * @author
+ * @author
+ */
+public class Trie {
+
+	private Node root;
+	
+	/**
+	 * Constructor
+	 */
+	public Trie() {
+		root = new Node ();
+	}
+	
+	/**
+	 * 
+	 */
+	public void sort () {
+		
+	}
+	
+	/**
+	 * Remove a node of the tree
+	 */
+	public void remove () {
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public void rearranje () {
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public boolean search (String key) {
+		return true;
+	}
+
+
+}
