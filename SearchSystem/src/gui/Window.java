02b406d04f16eb4824b29926752097014b28606d
Valmir Correa da Silva Junior <valmir.correa@imd.local>
2017-12-01 17:36:11 -0300

    inicinado a GUI

--- SearchSystem/src/gui/Window.java
+++ SearchSystem/src/gui/Window.java
@@ -3,24 +3,56 @@
  */
 package gui;
 
-import javax.swing.JWindow;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import java.awt.Toolkit;
 
 /**
  * The GUI elements generator
- * @author
+ * @author4 Valmir Correa
  *
  */
-public class Window extends JWindow {
+public class Window extends JFrame {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private JTextField textField;
+	/**
+	 * @wbp.nonvisual location=199,119
+	 */
+	private final ImageIcon imageIcon = new ImageIcon();
+	
+	public static void main (String args[]) {
+		Window teste = new Window ();
+		teste.setVisible(true);
+	}
 
 	/**
 	 * 
 	 */
 	public Window() {
+		
+		setBounds(500, 300, 450, 300);		// posição de inicio
+		
+		imageIcon.setImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\valmir.correa\\Downloads\\toogle.jpg"));
+		getContentPane().setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(84, 115, 256, 20);
+		getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JButton btnBuscaAnd = new JButton("Busca AND");
+		btnBuscaAnd.setBounds(106, 146, 105, 23);
+		getContentPane().add(btnBuscaAnd);
+		
+		JButton btnBuscaOr = new JButton("Busca OR");
+		btnBuscaOr.setBounds(221, 146, 105, 23);
+		getContentPane().add(btnBuscaOr);
 		// TODO Auto-generated constructor stub
 	}
-
 }
