fcf6df271f3214259dc5a009c37142f97bf23fe8
Valmir Correa <valmir.correa@ufrn.edu.br>
2017-12-04 00:42:38 -0300

    Adicionando comentários

--- SearchSystem/src/gui/Window.java
+++ SearchSystem/src/gui/Window.java
@@ -1,5 +1,5 @@
 /**
- * 
+ * Package to generate a user interface
  */
 package gui;
 
@@ -30,11 +30,9 @@ import java.awt.event.ActionEvent;
  *
  */
 public class Window extends JFrame {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	private JTextField textField;
+	
 	/**
 	 * @wbp.nonvisual location=199,149
 	 */
@@ -46,7 +44,7 @@ public class Window extends JFrame {
 	}
 
 	/**
-	 * 
+	 * Default builder for initializing interface components
 	 */
 	public Window() {
 		
@@ -106,6 +104,9 @@ public class Window extends JFrame {
 		// TODO Auto-generated constructor stub
 	}
 	
+	/**
+	* Perform "AND" Search
+	*/
 	private void buscaAnd () {
 //		try {
 //			String palvra = textField.getText();
@@ -115,10 +116,16 @@ public class Window extends JFrame {
 //		}
 	}
 	
+	/**
+	* Perform "OR" Search
+	*/
 	private void buscaOr () {
 		
 	}
 	
+	/**
+	* Add file to database
+	*/
 	private void adicionarArquivo () {
 		JFileChooser chooser = new JFileChooser();
 		FileNameExtensionFilter filter = new FileNameExtensionFilter ("*.txt","txt");
@@ -137,16 +144,18 @@ public class Window extends JFrame {
 		else if (returnVal == JFileChooser.CANCEL_OPTION) {
 			JOptionPane.showMessageDialog(null, "Cancelado!");
 		}
-		readArchive(file.getName());
+		addFile(file.getName());
 	}
 	
-	public void readArchive(String file) {
+	/**
+	* Add the file
+	*/
+	public void addFile(String file) {
 		Indexer index = new Indexer();
 		try {
 			index.addDocument(file);
 		} catch (FileTypeException | FileAlreadyExistsException e2) {
 			e2.printStackTrace();
 		}
-
 	}
 }
